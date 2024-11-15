ef49fdbb208408c116eb6444ddefcfdbb1c3c963
xororist <hcachon@myges.fr>
2024-11-15 11:31:27 +0100

    add base business logic in catalog api and building blocks shared between services

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommandandler<in TCommnad> 
+    : ICommandHandler<TCommnad, Unit>
+    where TCommnad : ICommand<Unit>
+{
+}
+
+public interface ICommandHandler<in TCommand, TReponse>
+    : IRequestHandler<TCommand, TReponse> 
+    where TCommand : ICommand<TReponse>
+    where TReponse : notnull
+{
+}
